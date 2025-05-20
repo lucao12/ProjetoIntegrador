de58f83c9b606a680fa52f37c217bd8053e69fca
Lucas Miranda <lucasmo652@gmail.com>
2025-05-20 00:33:46 -0300

    Melhorias no css na tela de login e senha

--- obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb876385047285f002d351630709efc7fd9b5f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5019b43709f006bdc0d9fd7a006c27e5a2987a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
